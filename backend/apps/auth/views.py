b744ac2893890354e580237b3d8eb5e4cce1bb8e
Oksana <oksanakysylychak@gmail.com>
2025-09-22 19:32:07 +0300

    base confs

--- backend/apps/auth/views.py
+++ backend/apps/auth/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.template.context_processors import request
 
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from core.services.jwt_service import ActivateToken, JWTService
+from core.services.email_service import EmailService
+from core.services.jwt_service import ActivateToken, JWTService, RecoveryToken, UserModel
 
+from apps.auth.serializers import EmailSerializer, PasswordSerializer
 from apps.user.serializer import UserSerializer
 
 
@@ -21,3 +24,25 @@ class ActivateUserView(GenericAPIView):
         serializer = UserSerializer(user)
         return Response(serializer.data, status.HTTP_200_OK)
 
+class RecoveryRequestView(GenericAPIView):
+    permission_classes = (AllowAny, )
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        serializer = EmailSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        user = get_object_or_404(UserModel, email=serializer.data['email'])
+        EmailService.recovery(user)
+        return Response({'details': 'Link send to email'}, status.HTTP_200_OK)
+
+class RecoveryPasswordView(GenericAPIView):
+    permission_classes = (AllowAny, )
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        serializer = PasswordSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        token = kwargs['token']
+        user = JWTService.verify_token(token, RecoveryToken)
+        user.set_password(serializer.data['password'])
+        user.save()
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
