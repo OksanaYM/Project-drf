214f8e42803e5afb08d1d9f632f38a6ff9a99c05
Oksana <oksanakysylychak@gmail.com>
2025-09-29 19:16:33 +0300

    base confs

--- backend/configs/extra_config/celery_conf.py
+++ backend/configs/extra_config/celery_conf.py
@@ -3,4 +3,6 @@ CELERY_RESULTS_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
