498005c57597c2587509e49758da4c180c84d6e7
Oksana <oksanakysylychak@gmail.com>
2025-10-06 09:48:33 +0300

    base confs

--- backend/configs/asgi.py
+++ backend/configs/asgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.middleware import BaseMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from core.middleware.socket_middleware import AuthSocketMiddleware
 
